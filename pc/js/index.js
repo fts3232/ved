32e569496038bf7d3de33f45126eb728ac0d7c4a
fts3232 <550610606@qq.com>
2015-11-05 14:33:26 +0800

    滚轮事件

--- pc/js/index.js
+++ pc/js/index.js
@@ -11,5 +11,36 @@ win.resize(function(){
 	resize();
 });
 $(document).ready(function(){
-	
+	page1=0
+		page2=$('.Center-Container').eq(0).height()
+		page3=page2+$('.Center-Container').eq(1).height()
+		page4=page3+$('.Center-Container').eq(2).height()
+		$('body').mousewheel(function(event, delta, deltaX, deltaY) {  
+		    if(delta==1){
+		    	total=$('body').scrollTop()
+		    	if(total==page2){
+		    		$('body').stop().animate({'scrollTop':page1+'px'});
+		    	}
+
+		    	else if(total==page3){
+		    		$('body').stop().animate({'scrollTop':page2+'px'});
+		    	}
+		    	else if(total<page4){
+		    		$('body').stop().animate({'scrollTop':page3+'px'});
+		    	}
+		    	
+		    }
+		    else{
+				total=$('body').scrollTop()
+				if(total<page2){
+		    		$('body').stop().animate({'scrollTop':page2+'px'});
+		    	}
+		    	else if(total<page3){
+		    		$('body').stop().animate({'scrollTop':page3+'px'});
+		    	}
+		    	else if(total>page4-300){
+		    		$('body').stop().animate({'scrollTop':page4+'px'});
+		    	}
+		    }	
+		});  
 })
